aa1357ddf9b14b52490cae19effe2e79350c3f79
Haidir Aditya <haidiraditya16@gmail.com>
2025-03-15 20:45:35 +0800

    first commit

---
+++ tests/hello.test.ts
@@ -0,0 +1,9 @@
+describe('Hello', () => {
+    it('should say hello', () => {
+        const name = "somwan";
+
+        // KUNCINYA ITU ADA DISINI
+         expect(name).toBe("somwan");
+    });
+
+});
